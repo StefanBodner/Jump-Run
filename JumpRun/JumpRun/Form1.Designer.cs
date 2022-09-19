72d5f5a88074903afddb43688c1ad549b61eaadb
Stefan Bodner <stefan.bodner@hak-kitz.ac.at>
2022-09-19 16:39:44 +0200

    Basic Mechanics & World Creation

--- JumpRun/JumpRun/Form1.Designer.cs
+++ JumpRun/JumpRun/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JumpRun
 {
-    partial class Form1
+    partial class frm_game
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,11 +29,85 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.tmr_game = new System.Windows.Forms.Timer(this.components);
+            this.lbl_left = new System.Windows.Forms.Label();
+            this.lbl_right = new System.Windows.Forms.Label();
+            this.lbl_up = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.player.Location = new System.Drawing.Point(236, 289);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 100);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // tmr_game
+            // 
+            this.tmr_game.Enabled = true;
+            this.tmr_game.Interval = 10;
+            this.tmr_game.Tick += new System.EventHandler(this.tmr_game_Tick);
+            // 
+            // lbl_left
+            // 
+            this.lbl_left.AutoSize = true;
+            this.lbl_left.Location = new System.Drawing.Point(821, 9);
+            this.lbl_left.Name = "lbl_left";
+            this.lbl_left.Size = new System.Drawing.Size(24, 15);
+            this.lbl_left.TabIndex = 1;
+            this.lbl_left.Text = "left";
+            // 
+            // lbl_right
+            // 
+            this.lbl_right.AutoSize = true;
+            this.lbl_right.Location = new System.Drawing.Point(821, 24);
+            this.lbl_right.Name = "lbl_right";
+            this.lbl_right.Size = new System.Drawing.Size(32, 15);
+            this.lbl_right.TabIndex = 2;
+            this.lbl_right.Text = "right";
+            // 
+            // lbl_up
+            // 
+            this.lbl_up.AutoSize = true;
+            this.lbl_up.Location = new System.Drawing.Point(821, 39);
+            this.lbl_up.Name = "lbl_up";
+            this.lbl_up.Size = new System.Drawing.Size(21, 15);
+            this.lbl_up.TabIndex = 3;
+            this.lbl_up.Text = "up";
+            // 
+            // frm_game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.lbl_up);
+            this.Controls.Add(this.lbl_right);
+            this.Controls.Add(this.lbl_left);
+            this.Controls.Add(this.player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frm_game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JumpRun";
+            this.Load += new System.EventHandler(this.frm_game_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frm_game_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox player;
+        private System.Windows.Forms.Timer tmr_game;
+        private Label lbl_left;
+        private Label lbl_right;
+        private Label lbl_up;
     }
 }
--- JumpRun/JumpRun/Form1.cs
+++ JumpRun/JumpRun/Form1.cs
@@ -1,10 +1,152 @@
 namespace JumpRun
 {
-    public partial class Form1 : Form
+    public partial class frm_game : Form
     {
-        public Form1()
+        public frm_game()
         {
             InitializeComponent();
+        }
+        private void frm_game_Load(object sender, EventArgs e)
+        {
+            WorldCreation();
+        }
+
+        #region Variables   
+        static bool left = false;
+        static bool right = false;
+        static bool up = false;
+        static int playerSpeed = 5;
+
+        static int gravity = -20;
+
+        static int boxHeight = 50;
+        static int boxWidth = 50;
+        #endregion
+
+
+        #region Worlds
+        static int[,] world = new int[,] {
+            { 1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,0,0,0,0 },
+            { 1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,0,0,0,0,0 },
+            { 1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            { 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1 },
+            { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 }};
+        #endregion
+
+
+        #region Movement
+        private void tmr_game_Tick(object sender, EventArgs e)
+        {
+            Movement();
+            Collision();
+            DebugVariables();
+        }
+
+        public void Movement()
+        {
+            if (left)
+            {
+                player.Left -= playerSpeed;
+            }
+            else if (right)
+            {
+                player.Left += playerSpeed;
+            }
+
+            //if (up)
+            //{
+            //    player.Top += force
+            //}
+
+            //if ()
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.A))
+            {
+                left = true;
+            }
+            else if (e.KeyCode.Equals(Keys.D))
+            {
+                right = true;
+            }
+            if (e.KeyCode.Equals(Keys.W))
+            {
+                up = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.A))
+            {
+                left = false;
+            }
+            else if (e.KeyCode.Equals(Keys.D))
+            {
+                right = false;
+            }
+            if (e.KeyCode.Equals(Keys.W))
+            {
+                up = false;
+            }
+        }
+
+        private void Collision()
+        {
+            //Bottom Collision
+            Rectangle colBot = new Rectangle(player.Left, player.Bottom, player.Width, 1);
+
+            this.CreateGraphics().FillRectangle(Brushes.Red, colBot);
+
+
+            if (colBot.IntersectsWith())
+            {
+
+            }
+        }
+        #endregion
+
+        #region World Creation
+        public void WorldCreation()
+        {
+            for (int i = 0; i < world.GetLength(0); i++)
+            {
+                for (int j = 0; j < world.GetLength(1); j++)
+                {
+                    if (world[i, j] == 1)
+                    {
+                        PictureBox pb = new PictureBox();
+                        pb.Top = i * boxHeight;
+                        pb.Left = j * boxWidth;
+                        pb.Height = boxHeight;
+                        pb.Width = boxWidth;
+                        pb.BackColor = Color.Blue;
+                        this.Controls.Add(pb);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Debug
+        public void DebugVariables()
+        {
+            lbl_left.Text = "left: " + left.ToString();
+            lbl_right.Text = "right: " + right.ToString();
+            //lbl_left.Text = "up: " + up.ToString();
+        }
+        #endregion
+
+        private void frm_game_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
